37db818a34ea593804152291672c05cc25f64ca0
jrekCody <jayrek_tabasa@cody.inc>
2024-03-26 16:04:37 +0800

    update sign in screen, apply button and image

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_zoom/gen/assets.gen.dart';
 import 'package:go_router/go_router.dart';
 
 import '../utils/enum/app_route_enum.dart';
@@ -9,11 +10,43 @@ class SignInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          child: const Text('sign in'),
-          onPressed: () => context.goNamed(AppRouteEnum.dashboard.name),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Start or join a meeting',
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30),
+            child: Image.asset(Assets.image.onboarding.path),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(18),
+            child: ElevatedButton(
+              onPressed: () => context.goNamed(AppRouteEnum.dashboard.name),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromRGBO(14, 114, 236, 1),
+                  minimumSize: const Size(
+                    double.infinity,
+                    50,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  )),
+              child: const Text(
+                'Google Sign In',
+                style: TextStyle(
+                  fontSize: 17,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
