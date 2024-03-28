ab846ac1bcdcec6ac856b181426449839b21b2d9
jrekCody <jayrek_tabasa@cody.inc>
2024-03-28 11:37:52 +0800

    add bottom navigation bar menus with StatefulNavigationShell

--- lib/feature/dashboard_screen.dart
+++ lib/feature/dashboard_screen.dart
@@ -1,14 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class DashboardScreen extends StatelessWidget {
-  const DashboardScreen({super.key});
+  const DashboardScreen({
+    required this.navigationShell,
+    super.key,
+  });
+
+  final StatefulNavigationShell navigationShell;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('dashboard'),
+      body: navigationShell,
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.comment_bank),
+            label: 'Meet & Chat',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.lock_clock),
+            label: 'Meetings',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person_outline),
+            label: 'Contacts',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.settings_outlined),
+            label: 'Settings',
+          ),
+        ],
+        selectedIndex: navigationShell.currentIndex,
+        onDestinationSelected: (index) => _onDestinationSelected(
+          context,
+          index,
+        ),
       ),
+    );
+  }
+
+  void _onDestinationSelected(BuildContext context, int index) {
+    navigationShell.goBranch(
+      index,
+      initialLocation: index == navigationShell.currentIndex,
     );
   }
 }
