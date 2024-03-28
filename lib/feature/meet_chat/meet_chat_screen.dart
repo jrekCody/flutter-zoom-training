3cbdae9252f64652c9285717f3609568e3aae6f4
jrekCody <jayrek_tabasa@cody.inc>
2024-03-28 12:16:12 +0800

    update meet chat screen

--- lib/feature/meet_chat/meet_chat_screen.dart
+++ lib/feature/meet_chat/meet_chat_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../widgets/zoom_feature_button_widget.dart';
+
 class MeetChatScreen extends StatelessWidget {
   const MeetChatScreen({super.key});
 
@@ -7,8 +9,48 @@ class MeetChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Meet & Chat'),
+          title: const Text('Meet & Chat'),
+          elevation: 0,
         ),
-        body: Center(child: Text('Meet & Chat')));
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ZoomFeatureButtonWidget(
+                  icon: Icons.videocam,
+                  label: 'New Meeting',
+                  onPressed: () {},
+                ),
+                ZoomFeatureButtonWidget(
+                  icon: Icons.add_box_rounded,
+                  label: 'Join Meeting',
+                  onPressed: () {},
+                ),
+                ZoomFeatureButtonWidget(
+                  icon: Icons.add_box_rounded,
+                  label: 'Schedule',
+                  onPressed: () {},
+                ),
+                ZoomFeatureButtonWidget(
+                  icon: Icons.add_box_rounded,
+                  label: 'Share Screen',
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            const Expanded(
+              child: Center(
+                child: Text(
+                  'Create/Join Meetings with just a click!',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
