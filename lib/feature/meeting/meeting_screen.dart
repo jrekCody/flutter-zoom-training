cce30a28a5ce539aaafedbc4dc655a6b062685de
jrekCody <jayrek_tabasa@cody.inc>
2024-03-29 23:24:07 +0800

    trigger ShowMeetingHistory display list of meetings realtime

--- lib/feature/meeting/meeting_screen.dart
+++ lib/feature/meeting/meeting_screen.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_zoom/feature/meeting/bloc/meeting_bloc.dart';
+import 'package:flutter_zoom/utils/datetime_util.dart';
+import 'package:flutter_zoom/utils/string_util.dart';
 
 class MeetingScreen extends StatelessWidget {
   const MeetingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    context.read<MeetingBloc>().add(const ShowMeetingHistory());
     return Scaffold(
         appBar: AppBar(
-          title: Text('Meetings'),
+          title: const Text(StringUtil.meeting),
         ),
-        body: Center(child: Text('Meetings')));
+        body: BlocBuilder<MeetingBloc, MeetingState>(
+          builder: (context, state) {
+            if (state.meeting.isNotEmpty) {
+              return ListView.separated(
+                itemCount: state.meeting.length,
+                itemBuilder: (context, index) {
+                  final rooms = state.meeting[index];
+                  return ListTile(
+                    title: Text('Room Name: ${rooms.roomName}'),
+                    subtitle: Text(
+                        'Joined on ${DateTimeUtil.formattedDateYMMMd(rooms.createdAt)}'),
+                  );
+                },
+                separatorBuilder: (_, __) => const Divider(
+                  height: 0.5,
+                  indent: 15,
+                  endIndent: 15,
+                ),
+              );
+            } else {
+              return const Center(
+                child: Text('no room meetings'),
+              );
+            }
+          },
+        ));
   }
 }
