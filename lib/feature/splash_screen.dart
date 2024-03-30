644f2d33571b58ecc7bd30813313eea48b4ad4ae
jrekCody <jayrek_tabasa@cody.inc>
2024-03-30 15:25:12 +0800

    refactor app route enum meetChat to newMeet

--- lib/feature/splash_screen.dart
+++ lib/feature/splash_screen.dart
@@ -15,7 +15,7 @@ class SplashScreen extends StatelessWidget {
       body: BlocConsumer<AuthBloc, AuthState>(
         listener: (context, state) {
           if (state is Authenticated) {
-            context.goNamed(AppRouteEnum.meetChat.name);
+            context.goNamed(AppRouteEnum.newMeet.name);
           }
         },
         builder: (context, state) => const SignInScreen(),
