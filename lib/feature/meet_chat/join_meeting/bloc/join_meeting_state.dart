0ce966c959e5d9deb77f28cedc419781f2be47b8
jrekCody <jayrek_tabasa@cody.inc>
2024-03-30 22:24:01 +0800

    update bloc, event, state for the join meeting feature

--- lib/feature/meet_chat/join_meeting/bloc/join_meeting_state.dart
+++ lib/feature/meet_chat/join_meeting/bloc/join_meeting_state.dart
@@ -1,10 +1,37 @@
 part of 'join_meeting_bloc.dart';
 
-sealed class JoinMeetingState extends Equatable {
-  const JoinMeetingState();
-}
+final class JoinMeetingState extends Equatable {
+  const JoinMeetingState({
+    this.roomName = '',
+    this.userName = '',
+    this.isAudioMuted = false,
+    this.isVideoOff = false,
+  });
 
-final class JoinMeetingInitial extends JoinMeetingState {
+  final String roomName;
+  final String userName;
+  final bool isAudioMuted;
+  final bool isVideoOff;
+
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [
+        roomName,
+        userName,
+        isAudioMuted,
+        isVideoOff,
+      ];
+
+  JoinMeetingState copyWith({
+    String? roomName,
+    String? userName,
+    bool? isAudioMuted,
+    bool? isVideoOff,
+  }) {
+    return JoinMeetingState(
+      roomName: roomName ?? this.roomName,
+      userName: userName ?? this.userName,
+      isAudioMuted: isAudioMuted ?? this.isAudioMuted,
+      isVideoOff: isVideoOff ?? this.isVideoOff,
+    );
+  }
 }
