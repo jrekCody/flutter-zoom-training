962a268d1baf4f0a145be8ca7a00f7ef9b9418cb
jrekCody <jayrek_tabasa@cody.inc>
2024-03-30 16:04:08 +0800

    add new string constant

--- lib/feature/meeting/meeting_screen.dart
+++ lib/feature/meeting/meeting_screen.dart
@@ -26,9 +26,9 @@ class MeetingScreen extends StatelessWidget {
                 itemBuilder: (context, index) {
                   final rooms = state.meeting[index];
                   return ListTile(
-                    title: Text('Room Name: ${rooms.roomName}'),
+                    title: Text('${StringUtil.roomName} ${rooms.roomName}'),
                     subtitle: Text(
-                        'Joined on ${DateTimeUtil.formattedDateYMMMd(rooms.createdAt)}'),
+                        '${StringUtil.joinedOn} ${DateTimeUtil.formattedDateYMMMd(rooms.createdAt)}'),
                   );
                 },
                 separatorBuilder: (_, __) => const Divider(
--- lib/utils/string_util.dart
+++ lib/utils/string_util.dart
@@ -11,5 +11,12 @@ final class StringUtil {
   static const String joinMeeting = 'Join Meeting';
   static const String schedule = 'Schedule';
   static const String shareScreen = 'Share Screen';
-  static const String createJoinMeeting = 'Create/Join Meetings with just a click!';
+  static const String createJoinMeeting =
+      'Create/Join Meetings with just a click!';
+  static const String roomName = 'Room Name';
+  static const String name = 'Name';
+  static const String join = 'Join';
+  static const String joinedOn = 'Joined on';
+  static const String muteAudio = 'Mute Audio';
+  static const String offVideo = 'Turn off video';
 }
