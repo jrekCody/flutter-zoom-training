1e3ec8cb255282e678878adfffa13f207335ff84
jrekCody <jayrek_tabasa@cody.inc>
2024-04-02 23:10:31 +0800

    update return base64 length

--- lib/utils/extension/string_extension.dart
+++ lib/utils/extension/string_extension.dart
@@ -5,6 +5,7 @@ extension StringExtensions on String {
   static String getRandomString(int len) {
     final random = Random.secure();
     final values = List<int>.generate(len, (i) => random.nextInt(255));
-    return base64UrlEncode(values);
+    final base64 = base64UrlEncode(values);
+    return base64.substring(0, len);
   }
 }
